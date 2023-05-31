af970015992db8def51869b5f6069146ff611d32
YaelavilaC <135137147+YaelavilaC@users.noreply.github.com>
2023-05-31 08:19:11 -0600

    Add files via upload

---
+++ Leccion2_2/src/com/pp2/app.java
@@ -0,0 +1,29 @@
+package com.pp2;
+
+public class app {
+
+
+	    public static void drawLegs(){
+	        System.out.println("     ||     ||      ");
+	        System.out.println("     ||     ||      ");
+	        System.out.println("    (||)   (||)     ");
+	    }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	        System.out.println("   /\\         /\\   ");
+	        System.out.println("  /  \\_______/  \\  ");
+	        System.out.println(" /               \\ ");
+	        System.out.println("(  /\\         /\\  )");
+	        System.out.println("====     V     ====");
+	        System.out.println("======(__|__)======");
+	        System.out.println("  (             )  ");
+	        System.out.println("   (___________)   ");
+	        drawLegs();
+	    }    
+		
+
+	}
+
+
