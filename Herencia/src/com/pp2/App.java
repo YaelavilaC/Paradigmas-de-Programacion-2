c30d2e9d2ec479c3b902e33b14612d95d35ea053
YaelavilaC <135137147+YaelavilaC@users.noreply.github.com>
2023-05-31 08:15:14 -0600

    Add files via upload

---
+++ Herencia/src/com/pp2/App.java
@@ -0,0 +1,13 @@
+package com.pp2;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Student s = new Student (26, "Kevin", "MIT");
+		s.showAge();
+
+	}
+
+}
