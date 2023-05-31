c2342d1ec4a48beccf8aff44dc9169e58656932a
YaelavilaC <135137147+YaelavilaC@users.noreply.github.com>
2023-05-31 08:08:01 -0600

    Add files via upload

--- App.java
+++ App.java
@@ -1,51 +1,29 @@
 package com.pp2;
 
+class Runner1 implements Runnable {
+	@Override
+	public void run () {
+		for(int i=0;i<10;i++)
+			System.out.println("Runner1: " + i);
+	}
+	
+}
+class Runner2 implements Runnable {
+	@Override
+	public void run () {
+		for(int i=0;i<10;i++)
+			System.out.println("Runner2: " + i);
+	}
+	
+}
 public class App {
 
-		private static int count1 = 0;
-		private static int count2 = 0;
-		
-		public synchronized static void add() {
-			count1++;
-		}
-		
-		public synchronized static void addAgain() {
-			count2++;
-		}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread t1 = new Thread(new Runner1());
+		Thread t2 = new Thread(new Runner2());
+		t1.start();
+		t2.start();
+	}
 
-		public static void compute() {
-			for(int i=0;i<100;++i) {
-				add();
-				addAgain();
-			}
-		}
-		
-		public static void main(String[] args) {
-
-			Thread t1 = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					compute();
-				}
-			});
-			
-			Thread t2 = new Thread(new Runnable() {
-				@Override
-				public void run() {
-					compute();
-				}
-			});
-			
-			t1.start();
-			t2.start();
-			
-			try {
-				t1.join();
-				t2.join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			System.out.println("Count1="+count1+" - Count2="+count2);
-		}
 }
